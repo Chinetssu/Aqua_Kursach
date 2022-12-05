58cbeb2d10913357475c83c2b677a3537843ebbd
даниил дикальчук <sana.dana.81@hotmail.com>
2022-12-05 13:14:16 +0800

    Только добавил класс ImpactPoint

---
+++ Aqua_Kursach/Emitter.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Aqua_Kursach
+{
+    class Emitter
+    {
+
+        public List<Particle> particles = new List<Particle>();
+        public List<Point> gravityPoints = new List<Point>(); // тут буду хранится точки притяжения
+
+
+        public float GravitationX = 0;
+        public float GravitationY = 1; // пусть гравитация будет силой один пиксель за такт, нам хватит
+        public int MousePositionX;
+        public int MousePositionY;
+        public void UpdateState()
+        {
+            foreach (var particle in particles)
+            {
+                particle.Life -= 1; // уменьшаю здоровье
+                                    // если здоровье кончилось
+                if (particle.Life < 0)
+                {
+                    // восстанавливаю здоровье
+                    particle.Life = 20 + Particle.rand.Next(100);
+                    // перемещаю частицу в центр
+                    particle.X = MousePositionX;
+                    particle.Y = MousePositionY;
+                    var direction = (double)Particle.rand.Next(360);
+                    var speed = 1 + Particle.rand.Next(10);
+
+                    particle.SpeedX = (float)(Math.Cos(direction / 180 * Math.PI) * speed);
+                    particle.SpeedY = -(float)(Math.Sin(direction / 180 * Math.PI) * speed);
+                    particle.Radius = 2 + Particle.rand.Next(10);
+                }
+                else
+                {
+                    // каждая точка по-своему воздействует на вектор скорости
+                    foreach (var point in gravityPoints)
+                    {
+                        float gX = point.X - particle.X;
+                        float gY = point.Y - particle.Y;
+                        float r2 = (float)Math.Max(100, gX * gX + gY * gY); // ограничил
+
+                        float M = 100;
+
+                        particle.SpeedX += (gX) * M / r2;
+                        particle.SpeedY += (gY) * M / r2;
+                    }
+
+                    // гравитация воздействует на вектор скорости, поэтому пересчитываем его
+                    particle.SpeedX += GravitationX;
+                    particle.SpeedY += GravitationY;
+
+                    particle.X += particle.SpeedX;
+                    particle.Y += particle.SpeedY;
+                }
+            }
+            for (var i = 0; i < 10; ++i)
+            {
+                if (particles.Count < 500) // пока частиц меньше 500 генерируем новые
+                {
+                    var particle = new ParticleColorful();
+                    particle.FromColor = Color.Yellow;
+                    particle.ToColor = Color.FromArgb(0, Color.Magenta);
+                    particle.X = MousePositionX;
+                    particle.Y = MousePositionY;
+                    particles.Add(particle);
+                }
+                else
+                {
+                    break; // а если частиц уже 500 штук, то ничего не генерирую
+                }
+            }
+        }
+
+        public void Render(Graphics g)
+        {
+            // утащили сюда отрисовку частиц
+            foreach (var particle in particles)
+            {
+                particle.Draw(g);
+            }
+            // рисую точки притяжения красными кружочками
+            foreach (var point in gravityPoints)
+            {
+                g.FillEllipse(
+                    new SolidBrush(Color.Red),
+                    point.X - 5,
+                    point.Y - 5,
+                    10,
+                    10
+                );
+            }
+        }
+    }
+}
--- Aqua_Kursach/Form1.cs
+++ Aqua_Kursach/Form1.cs
@@ -12,9 +12,47 @@ namespace Aqua_Kursach
 {
     public partial class Form1 : Form
     {
+        Emitter emitter = new Emitter(); // добавили эмиттер
+        List<Particle> particles = new List<Particle>();
         public Form1()
         {
             InitializeComponent();
+
+            picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
+            emitter.gravityPoints.Add(new Point(
+        picDisplay.Width / 2, picDisplay.Height / 2
+    ));
+
+            // добавил еще две        
+            emitter.gravityPoints.Add(new Point(
+              (int)(picDisplay.Width * 0.75), picDisplay.Height / 2
+           ));
+
+            emitter.gravityPoints.Add(new Point(
+               (int)(picDisplay.Width * 0.25), picDisplay.Height / 2
+           ));
+        }
+
+
+        int counter = 0; // добавлю счетчик чтобы считать вызовы функции
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            emitter.UpdateState(); // тут теперь обновляем эмиттер
+
+            using (var g = Graphics.FromImage(picDisplay.Image))
+            {
+                g.Clear(Color.White);
+                emitter.Render(g); // рендерим систему
+            }
+
+            picDisplay.Invalidate();
+        }
+
+        private void picDisplay_MouseMove(object sender, MouseEventArgs e)
+        {
+            emitter.MousePositionX = e.X;
+            emitter.MousePositionY = e.Y;
         }
     }
 }
